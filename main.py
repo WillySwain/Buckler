ebcecd0c9623db8f7b2a2a8c9d8eab0576f09867
Willy <williamswain361@gmail.com>
2022-07-21 01:07:15 -0400

    Typing words added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randrange
-
+import pygame.freetype
 import pygame
 from character import Character
 from boss import Boss
@@ -13,18 +13,23 @@ screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 pygame.display.set_caption("Buckler")
 clock = pygame.time.Clock();
 FPS = 60
-font = pygame.font.SysFont('pristina', 64)
 tutorialBoss = Boss(4)
-defaultCharacter = Character(["slam", "bam", "blow"])
-running = True
+defaultCharacter = Character(["slam", "slice", "bash"])
 index = randrange(0, 3)
-text = font.render(defaultCharacter.spellBook[index],True,(234,221,202))
-textRect = text.get_rect()
+text = defaultCharacter.spellBook[index]
+font = pygame.freetype.Font(None, 64)
+font.origin = True
+text_surf_rect = font.get_rect(text)
+baseline = text_surf_rect.y
+text_surf = pygame.Surface(text_surf_rect.size)
+metrics = font.get_metrics(text)
+
+running = True
+M_ADV_X = 4
 while running:
     clock.tick(FPS)
     tutorialBoss.draw(screen)
     defaultCharacter.draw(screen)
-    screen.blit(text, textRect)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
@@ -45,9 +50,32 @@ while running:
                 pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(900, 400, 60, 60))
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(800, 300, 60, 60))
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(700, 400, 60, 60))
+            if event.unicode == defaultCharacter.spellBook[index][defaultCharacter.curLetter].lower():
+                defaultCharacter.curLetter += 1
+                if defaultCharacter.curLetter>=len(text):
+                    defaultCharacter.curLetter = 0
+                    index = randrange(0, 3)
+                    text = defaultCharacter.spellBook[index]
+                    text_surf_rect = font.get_rect(text)
+                    baseline = text_surf_rect.y
+                    text_surf = pygame.Surface(text_surf_rect.size)
+                    metrics = font.get_metrics(text)
 
         if event.type == pygame.QUIT:
             running = False
+    x = 0
+    text_surf.fill((0, 0, 0))
+    for (idx, (letter, metric)) in enumerate(zip(defaultCharacter.spellBook[index], metrics)):
+        if idx == defaultCharacter.curLetter:
+            color = 'lightblue'
+        elif idx < defaultCharacter.curLetter:
+            color = 'lightgrey'
+        else:
+            color = 'white'
+        font.render_to(text_surf, (x, baseline), letter, color)
+        x += metric[M_ADV_X]
+
+    screen.blit(text_surf, text_surf_rect)
     pygame.display.update()
 
 pygame.quit()
